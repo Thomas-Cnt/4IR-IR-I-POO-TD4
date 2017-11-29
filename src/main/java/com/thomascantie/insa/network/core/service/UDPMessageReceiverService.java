59ce6ece7b643fa212ac823b3b46a2ed626548de
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-29 09:38:18 +0100

    -- send and recieve message with UDP

--- src/main/java/com/thomascantie/insa/network/core/service/UDPMessageReceiverService.java
+++ src/main/java/com/thomascantie/insa/network/core/service/UDPMessageReceiverService.java
@@ -1,8 +1,22 @@
 package com.thomascantie.insa.network.core.service;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class UDPMessageReceiverService implements MessageReceiverService {
+
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+		DatagramSocket socket = new DatagramSocket(port);
+
+		while(true) {
+			byte[] buffer = new byte[8196];
+
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+
+			incomingMessageListener.onNewIncomingMessage(new String(buffer));
+		}
     }
+
 }
--- src/main/java/com/thomascantie/insa/network/core/service/UDPMessageSenderService.java
+++ src/main/java/com/thomascantie/insa/network/core/service/UDPMessageSenderService.java
@@ -1,8 +1,20 @@
 package com.thomascantie.insa.network.core.service;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class UDPMessageSenderService implements MessageSenderService {
+
     @Override
     public void sendMessageOn(String ipAddress, int port, String message) throws Exception {
-        throw new UnsupportedOperationException();
+        DatagramSocket socket = new DatagramSocket();
+
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), InetAddress.getByName(ipAddress), port);
+
+        socket.send(packet);
+
+        socket.close();
     }
+
 }
