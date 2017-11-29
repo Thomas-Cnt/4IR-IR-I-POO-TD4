dc7078ae70793d4c669024a7affbc6efdb3b1cc7
Thomas-Cnt <26445883+Thomas-Cnt@users.noreply.github.com>
2017-11-29 10:18:58 +0100

    -- initial commit

---
+++ src/main/java/com/thomascantie/insa/network/core/service/AcceptClients.java
@@ -0,0 +1,42 @@
+package com.thomascantie.insa.network.core.service;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class AcceptClients implements Runnable {
+
+	private ServerSocket serverSocket;
+	private IncomingMessageListener incomingMessageListener;
+	private Socket socket;
+
+	public AcceptClients(ServerSocket socket, IncomingMessageListener listener){
+		this.serverSocket = socket;
+		this.incomingMessageListener = listener;
+	}
+
+	@Override
+	public void run() {
+
+		Socket socket;
+		try {
+			while(true){
+
+				socket = this.serverSocket.accept();
+
+				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				this.incomingMessageListener.onNewIncomingMessage(reader.readLine());
+
+				socket.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
